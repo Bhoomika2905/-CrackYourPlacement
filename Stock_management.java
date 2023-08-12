dc746696ea043b74817af5bdcd184e576a7340a4
N Bhoomika <87877276+Bhoomika2905@users.noreply.github.com>
2023-08-13 02:04:02 +0530

    Update Stock_management.java

--- Stock_management.java
+++ Stock_management.java
@@ -6,7 +6,7 @@ public class Stock_management{
         int lowest=prices[0];
         int high=-1;
         int low=0;
-        for (int i=0; i<n; i++) {
+        for (int i=0; i<n-1; i++) {
             if(prices[i]<lowest){
                 lowest=prices[i];
                 low=i;
